0a1f860a3d2567a7e2d8507db4a055e6937df5c6
DiegoAntonioCaceresAranda <u22212808@utp.edu.pe>
2026-08-28 20:20:02 -0500

    feat: added local data

---
+++ src/data/Categoria.ts
@@ -0,0 +1,12 @@
+interface Categoria {
+    id: number;
+    nombre: string;
+}
+
+const categoriesMock: Categoria[] = [
+    { id: 1, nombre: "Bebidas Calientes" },
+    { id: 2, nombre: "Postres" },
+    { id: 3, nombre: "Bebidas Frías" },
+];
+
+export default categoriesMock;
