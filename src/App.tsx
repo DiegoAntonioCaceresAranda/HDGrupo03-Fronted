a06bb58c41b0fe2ead2be7add8cd48397d44f332
DiegoAntonioCaceresAranda <u22212808@utp.edu.pe>
2026-09-14 20:31:33 -0500

    feat: test 2

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ import PaginaDashboard from "./pages/PaginaDashboard";
 import PaginaReportes from "./pages/PaginaReportes";
 import { CategoriasPage } from "./pages/CategoriaPage";
 import { ProductosPage } from "./pages/ProductosPage";
-import ClienteHome from "./pages/ClienteHome";
-import CartaPrincipal from "./pages/CartaPrincipal";
+/*import ClienteHome from "./pages/ClienteHome";
+import CartaPrincipal from "./pages/CartaPrincipal";*/
 import { UsuarioPage } from "./pages/UsuarioPage";
 import RolesPage from "./pages/RolPage";
 import PaginaVentas from "./pages/PaginaVentas";
